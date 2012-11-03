33a75b0c073a254b19fd669d95a8431883039437
Scott Young <scottyoung@unknown283737240eb4.gateway.2wire.net>
2012-11-02 20:57:14 -0700

    Parser connected

---
+++ src/Parser.py
@@ -0,0 +1,147 @@
+import robotparser
+from bs4 import BeautifulSoup
+
+class Parser:
+    """ 
+        Parser - recieves a document
+
+        return - a list of links from the page.
+        * Checks robots.txt for permission *
+    """
+    
+    # List of saved content files
+    #file_list = []
+
+    def __init__(self, site, tags):
+        """ Initializes the parser for a site address and retrieve robots.txt"""
+
+        self.site_url = site    # Site url received from the spider
+        self.link_list = []     # List of links
+        self.output = {}        # Dictionary of output by tag
+        self.tag_list = tags    # List of tags to search for content
+
+        if self.tag_list == None:
+            self.tag_list = ["p", "h1", "h2", "h3", "h4", "h5", "h6"]
+
+        # Download robots.txt
+        self.robots_txt = robotparser.RobotFileParser()
+        self.robots_txt.set_url(self.site_url)
+        self.robots_txt.read()
+
+    def run(self, doc):
+        """ Parses document 
+        
+            Input
+                doc - an html document
+        """
+
+        try:
+            
+            # Set up the beautifulsoup parser with lxml
+            # http://www.crummy.com/software/BeautifulSoup/
+            # http://lxml.de/
+            out = BeautifulSoup(doc, "lxml")
+
+            #output = {}
+        
+            
+            # Find content for specified tags
+            for tag in out.find_all(self.tag_list):
+                self.output[tag] = tag.text.encode('utf-8') 
+
+            # Find all the links
+            links = ''
+            for link in out.find_all('a'):
+                # Only add if okay by robots.txt
+                if self.robots_txt.can_fetch('*', str(link)):
+                    links += str(link.get('href')) + " \n"
+    
+            # Process the links so they are absolute urls
+            self.link_list = __process_links(links, self.site_url)
+
+            # Process the links so can be output to a file
+            #link_output = ''
+            #for link in self.link_list:
+            #   link_output += link + '\n'
+
+            # Save the content information for the page
+            #### TODO - fix the hardwired file paths
+            #outfile_name = "/home/project/Files/Content/" + doc_name + ".cnt"
+            #data_out = output.encode('utf-8')
+            #outfile = open(outfile_name, "w")
+            #outfile.write(data_out)
+
+            # Update the file list
+            #self.file_list += [outfile_name]
+
+            # Save the link information for the page
+            #linkfile_name = "Files/Links/" + doc_name + ".lk"
+            #linkfile = open(linkfile_name, "w")
+            #linkfile.write(link_output)
+            
+        #except IOError: # Error opening the file
+            #print("Problem opening page file: " + file_name)
+        
+        except: 
+            print("Problem parsing the Data")
+        
+        #finally: # Release the files
+            #outfile.close()
+            #linkfile.close()
+
+    def get_links(self):
+        """ Return the list of links found on the page. """
+        return self.link_list
+
+    def get_output(self):
+        """ Return the output found on the page. """
+        return self.output
+
+
+#   def get_file_list(self):
+#       """ Return the list of files """
+#       return self.file_list
+
+
+def __process_links(links, site):
+    """ 
+        Process links based upon site name so don't leave the site.
+        Returns a list of the links on a page.
+    """
+
+    # List to hold the new links
+    link_list = []
+
+    # Determine the site name from the site
+    # Remove www, https://, http://, .com, .org
+    temp1 = site.replace("www.", "")
+    temp2 = temp1.replace("https://", "")
+    temp3 = temp2.replace("http://", "")
+    temp4 = temp3.replace(".com", "")
+    site = temp4.replace(".org", "")
+
+    # Split the output into a list
+    list = links.split();
+
+    # Process all the links
+    for link in list:
+
+        # If absolute url
+        if link[0:3] == "http":
+            
+            # With site name add to list
+            if site in link:
+                
+                    link_list += [link]
+                    
+            # If absolute without site name skip
+        
+        # If relative add site
+        if (link[0] == '/' and len(link) > 1):
+            
+            link_abs = site + link
+            link_list += [link_abs]
+            
+            # Else skip, junk
+            
+    return link_list
---
+++ src/SpiderRunner.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+"""
+	Spider Runner
+
+    Downloads the first page of the given site and saves a number of links to 
+    a txt file. The number of links saved is the number of mappers that will
+    be run. Next, it sets up a redis data-structure server. The runner then
+    initiates parallel spider, passing all required info.
+"""
+import sys
+import redis
+import urllib
+
+from datetime import datetime
+from optparse import OptionParser
+
+from Parser import *
+
+class SpiderRunner:
+
+    def __init__(self, site_list, analysis_info, redis_info, max_mappers):
+
+        self.site_list = site_list 
+        self.analysis_info = analysis_info
+        self.redis_info = redis_info
+        self.max_mappers = max_mappers
+
+        # Create a time stamp (no querries should be issued in the same second)
+        d = datetime.now()
+        (y, m, d, h, mn) = (str(d.year), str(d.month), str(d.day), 
+                            str(d.hour), str(d.minute))
+        timestamp = y + "-" + m + "-" + d + "-" + h + "-" + mn
+
+        # Create a list of Redis base key parts based on time and site name
+        self.base_keys = []
+        for site in self.site_list:
+            base = site + "::" + timestamp
+            self.base_keys.append(base)
+ 
+
+           
+    def execute(self):
+        
+        #print self.timestamp
+        print self.site_list
+        for x in self.site_list:
+            print x
+        for y in self.base_keys:
+            print y
+
+        print self.redis_info
+
+        # Download initial page for each site
+        # Later make asynchronous
+        for site in self.site_list:
+            f = urllib.urlopen(site)
+            data = f.read()
+            f.close
+
+            # Hard code tags - later make variable of Analysis Info
+            tag_list = ["p", "h1", "h2", "h3", "h4", "h5", "h6"]
+            parser = Parser(site, tag_list)
+
+
+
+        # Connect to Redis
+        r = redis.StrictRedis(host=self.redis_info["host"],
+                              port=int(self.redis_info["port"]), db=0)
+
+        # Create keys for each site's link state: new, processing, finished
+        for key in self.base_keys:
+            new_links = key + "::new_links"
+            processing_links = key + "::processing_links"
+            finished_links = key + "::finished_links"
+
+        r.set("new_links","self.")
+        x = r.get("foo")
+        print x
+
+
+def main():
+    """ Run program from the command line. """
+
+    # Parse command line options and arguments.
+    usage = "usage: %prog [options] <site1url,site2url,...>"
+    parser = OptionParser(usage)
+
+    # Analysis info from the command line (x:y,a:b,wtf:true).
+    # Possibly add configuration file
+    parser.add_option(
+            "-a", "-A", "--analysisInfo", action="store", dest="analysisInfo", 
+            help="A comma separated list of analysis options.")
+
+    # Maximum number of mappers (controls download rate)
+    parser.add_option(
+            "-m", "-M", "--maxMappers", action="store", 
+            default=5, dest="maxMappers", 
+            help="Set maximum number of mappers. [default: %default]")
+
+    # Redis info: host and port
+    # redo defaulat localhost?
+    parser.add_option(
+            "-r", "-R", "--redisInfo", action="store",
+            dest="redisInfo", help="Set Redis info.")
+
+    # Argument is a comma separted list of site names
+    (options, args) = parser.parse_args()
+
+    # Make sure there is one and only one list of URLs
+    if len(args) == 0:
+        parser.error("Must specify list of site URLs")
+    if len(args) > 1:
+        parser.error("Can only specify 1 input list.")
+
+    # Convert string argument to Python list
+    site_list = args[0].split(",")
+
+    # Convert Analysis info to Python Dictionary
+    analysis_info = {}
+    if options.analysisInfo:
+        temp_list = options.analysisInfo.split(",")
+        for item in temp_list:
+            key, delimiter, value = item.partition('=')
+            analysis_info[key] = value
+
+    # Convert Redis info to Python Dictionary (keep default)
+    redis_info = {}
+    temp_list = options.redisInfo.split(",")
+    for item in temp_list:
+        key, delimiter, value = item.partition('=')
+        redis_info[key] = value
+
+    # Check max mappers is greater than 0
+    max_mappers = int(options.maxMappers)
+    if max_mappers < 1:
+        parser.error("maxMappers must be greater than 0")
+    
+    #  Initialize and execute spider runner
+    spider_runner = SpiderRunner(site_list, analysis_info, 
+                                 redis_info, max_mappers) 
+    spider_runner.execute()
+
+
+if __name__ == "__main__":
+    """ enable command line execution """
+    sys.exit(main())
+    
+
