7d5100a0c5fe5cccf6652868e7add647cdea5c38
Scott Young <scottyoung@unknown283737240eb4.gateway.2wire.net>
2012-11-28 16:24:34 -0800

    going again

--- src/ParallelSpider.py
+++ src/ParallelSpider.py
@@ -8,10 +8,17 @@
         particular analysis to be performed is included in the input, along 
         with information to connect to Redis, a data structure server used 
         to maintain link state.
+
+        * TODO: id mappers by incrementing a counter. *
+        * Use it to replace sharing temp keys *
 """
 class Mapper():
 
     def __init__(self):
+        """ Parallel Spider Mapper Constructor
+
+            Takes in parameter for Redis information
+        """
         import sys
                       
         # Convert Redis info to Python Dictionary
@@ -33,6 +40,13 @@ class Mapper():
     
     
     def __call__(self, key, value):
+        """ Parallel Spider Mapper 
+        
+            Sets up Redis information.
+            Loops downloading pages, feeding them to
+            an analysis engine which emits info to
+            the Reducer.  Stops when no more new links
+            or the max pages has been reached."""
         
         import redis
         import urllib
@@ -41,15 +55,9 @@ class Mapper():
         # Connect to Redis
         r = redis.StrictRedis(host=self.redis_info["host"],
                               port=int(self.redis_info["port"]), db=0)
-
-        # temporary - hard code key name
-        #base = 'http://www.foxnews.com/::2012-11-4-20-2'
-        #new_links = 'http://www.foxnews.com/::2012-11-4-10-10::new_links'
-        #site = "http://www.foxnews.com/"
         
         # Create keys for Redis
         base = self.redis_info["base"]
-        site, d, date = base.partition("::")    # determine site name
         new_links = base + "::new_links"        # new links
         processing = base + "::processing"      # links being processed
         finished = base + "::finished"          # links done being processed
@@ -57,93 +65,160 @@ class Mapper():
         temp1 = base + "::temp1"                # temp keys for set ops
         temp2 = base + "::temp2"
 
-        # Retrieve a a link
-        # later retrieve 5+? Asynchronously, batch process links to Redis
-        link = r.spop(new_links)
-
-        # Add link to processing
-        r.sadd(processing, link)
-
-        # Download page
-        f = urllib.urlopen(link)
-        data = f.read()
-        f.close
-
         # Hard code tags - later make variable of Analysis Info
         tag_list = ["p", "h1", "h2", "h3", "h4", "h5", "h6"]
+
+        # Set up Parser
+        site, d, date = base.partition("::")    # determine site name
         parser = Parser(site, tag_list)
-        parser.run(data)
-        links = parser.get_links()
-        output = parser.get_output() 
 
-        
-        #yield link, 1
+        stuff_to_scrape = True
+        #ti = 0 #TEST
 
-        # Simple word count of all tags' content
-        for tag in tag_list:
-            for word in output[tag].split():
-                yield word, 1
+        cnt = 0 # Holder for counter
 
-        #for word in value.split():
-        #    yield word, 1
+        # here we go...
+        while stuff_to_scrape:
 
-        # Add link to finished
-        r.sadd(finished, link)
+            # TODO: Fix - currently hardcoded to stop at particular count on line 212 
+            # Check conditions: still pages to download and not at max
+            #if (r.get(count) >= self.redis_info["maxPages"]): #or
+                #(r.exists(new_links) == False) or (r.scard(new_links) < 1)):
+            #    stuff_to_scrape = False
+            #    break
 
-        # increase total pages processed
-        r.incr(count)
+            # Try to pop a link
+            try:
 
-        # Remove from processing
-        r.srem(processing, link)
+                # Retrieve a a link
+                # later retrieve 5+? Asynchronously, batch process links to Redis
+                link = r.spop(new_links)
 
-        # Transaction to store new links
-        pipe = r.pipeline()
+                # Add link to processing
+                r.sadd(processing, link)
 
-        # Add any new links found
-        # Attempting to process as batch vice adding 1 at a time
-        # Can only add 255 elements max at a time
-        size = len(links)
+            # Alert that can't pop a link
+            except:
+                message = "Unable to pop a link"
+                yield message, 1
+                break
 
-        # Calculate number of breaks
-        breaks, remainder = divmod(size, 250)
-        if remainder > 0:   # Reminder: crack babies are sad.
-            breaks = breaks + 1
 
-        # Set initial indices
-        start = 0
-        finish = 250
-        i = 0
+            # Try to download and parse the page
+            try:
+            
+                # Download page
+                f = urllib.urlopen(link)
+                data = f.read()
+                f.close
 
-        while i < breaks:
-            links_part = links[start:finish]
+                # Parse the page and get the info
+                parser.run(data)
+                links = parser.get_links()
+                output = parser.get_output()
 
-            #Add single quotes around each element in list: map
-            #   Then combine elements to form a string for eval: join
-            link_string = (',').join(map(lambda x: "'" + x + "'",
-                                         links_part))
+            # Alert that can't parse and restart loop
+            except:
+                message = "Unable to parse: " + link
+                yield message, 1
+                continue
 
-            # ??? SECURITY ??? - site name is user provided
-            eval("pipe.sadd('" + temp1 + "'," + link_string + ")") 
+            # Try to process / emit information
+            try:
+            
+                # TEST
+                #yield "z_max_pages", int(self.redis_info["maxPages"])
+                # TEST - see links being processed
+                #yield link, 1
 
-            # Increment indices
-            start = start + 250
-            finish = finish + 250
-            i = i + 1
+                # Simple word count of all tags' content
+                for tag in tag_list:
+                    for word in output[tag].split():
+                        yield word, 1
 
-        # new links = temp - new links - processing - finished
-        pipe.sdiffstore(temp2, temp1, new_links, processing, finished)
-        pipe.sunionstore(new_links, new_links, temp2)
-        pipe.delete(temp1)
-        pipe.delete(temp2)
-        pipe.execute()
-       
+                #for word in value.split():
+                #    yield word, 1
+
+            # Alert that can't process info
+            except:
+                message = "Unable to process info for: " + link
+                yield message, 1
+                continue
+
+            # Try to finish processing link
+            try:
+
+                # Add link to finished
+                r.sadd(finished, link)
+
+                # increase total pages processed
+                r.incr(count)
+
+                # Remove from processing
+                r.srem(processing, link)
+
+                # Transaction to store new links
+                pipe = r.pipeline()
+
+                # Add any new links found
+                # Attempting to process as batch vice adding 1 at a time
+                # Can only add 255 elements max at a time
+                size = len(links)
+
+                # Calculate number of breaks
+                breaks, remainder = divmod(size, 250)
+                if remainder > 0:   # Reminder: crack babies are sad.
+                    breaks = breaks + 1
+
+                # Set initial indices
+                start = 0
+                finish = 250
+                i = 0
+
+                while i < breaks:
+                    links_part = links[start:finish]
+
+                    #Add single quotes around each element in list: map
+                    #   Then combine elements to form a string for eval: join
+                    link_string = (',').join(map(lambda x: "'" + x + "'",
+                                                 links_part))
+
+                    # ??? SECURITY ??? - site name is user provided
+                    eval("pipe.sadd('" + temp1 + "'," + link_string + ")") 
+
+                    # Increment indices
+                    start = start + 250
+                    finish = finish + 250
+                    i = i + 1
+
+                # new links = temp - new links - processing - finished
+                pipe.sdiffstore(temp2, temp1, new_links, processing, finished)
+                pipe.sunionstore(new_links, new_links, temp2)
+                pipe.delete(temp1)
+                pipe.delete(temp2)
+                pipe.execute()
+ 
+            # Alert that can't process info
+            except:
+                message = "Unable to finish processing: " + link
+                yield message, 1
+                continue          
+
+            cnt = int(r.get(count))
+            # TEST
+            msg = "z_count_" + str(cnt)
+            yield msg, 1
+            
+            # Goes over by number of mappers
+            if cnt > 10:
+                break
+
         # Set 1 hour expirations on all keys
         hour = 60 * 60 # 60 seconds/minute * 60 minutes
-        r.expire(new_links, hour)
-        r.expire(processing, hour)
-        r.expire(finished, hour)
-        r.expire(count, hour)
-
+        if r.exists(new_links): r.expire(new_links, hour)
+        if r.exists(processing): r.expire(processing, hour)
+        if r.exists(finished): r.expire(finished, hour)
+        if r.exists(count):r.expire(count, hour)
 
 def reducer(key, values):
     yield key, sum(values)
