44a434d45fdd3442e7ea1bfa42d21be30f9ee4bb
Scott Young <scottyoung@Scotts-MacBook-Air-2.local>
2013-01-11 17:25:47 -0800

    added angular

---
+++ spiderweb/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var spiderwebApp = angular.module('spiderwebApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
