037c8cc6caa2efbde93d5b758dcc62108541f686
Poof Young <poof@coding4kicks.com>
2013-07-08 23:02:57 -0700

    Add threaded command execution on master and nodes for engine in spiderdepot.

--- spiderdepot/engine.py
+++ spiderdepot/engine.py
@@ -5,6 +5,7 @@ import fabric.api as fab
 import os
 import sys
 import signal
+import threading
 import subprocess
 
 # Determine path to parallelspider directory.
@@ -15,13 +16,59 @@ path = os.path.realpath(__file__).partition('spiderdepot')[0]
 @fab.task
 def launch_new_scratch():
     """Start a spider engine from scratch, not an AMI."""
+
     # Initial commands must be synchronous
     # Use basecluster template
-    cmd = 'starcluster start -c basecluster basecluster'
-    p = subprocess.call(cmd, shell=True)
+    ###cmd = 'starcluster start -c basecluster basecluster'
+    ###p = subprocess.call(cmd, shell=True)
+    ###if p != 0:
+    ###    print "Starcluster failed to start."
+    ###    sys.exit(1)
+
+    # Create list of nodes
+    instance_list = []
+    nodes = 1
+    i = 1;
+    while i <= nodes:
+        if i < 10:
+            node_name = 'node00' + str(i)
+        elif i < 100:
+            node_name = 'node0' + str(i)
+        elif i < 1000:
+            node_name = 'node' + str(i)
+        else:
+            print "Too many nodes dumbass."
+            sys.exit(1)
+        instance_list.append(node_name)
+        i += 1
+
+    # Launch master and nodes
+    t = threading.Thread(target=init_config, args=('master', 'master'))
+    t.start()
+    for instance in instance_list:
+        t = threading.Thread(target=init_config, args=('node', instance))
+        t.start()
+    #print instance_list
+
+def init_config(instance_type=None, instance=None):
+    print 'here'
+    cmd_list = [
+            'sudo apt-get update'
+            ]
+    for cmd in cmd_list:
+        if instance_type == 'master':
+            full_cmd = 'starcluster sshmaster basecluster ' + cmd
+        elif instance_type == 'node':
+            full_cmd = 'starcluster sshnode basecluster node001 ' + cmd
+        else:
+            print('Error: Unknown Instance Type')
+            sys.exit(1)
+    p = subprocess.call(full_cmd, shell=True)
     if p != 0:
-        print "Starcluster failed to start."
+        print 'Command failed: ' + cmd
+        print 'For instance: ' + instance
         sys.exit(1)
+    print 'Command: ' + cmd + ' executed on ' + instance + '.'
 
 
 @fab.task
